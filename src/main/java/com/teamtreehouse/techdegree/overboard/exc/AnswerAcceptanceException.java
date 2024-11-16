3c28c1dc9d8236c6b786d59818892144630a2789
Leah Seyoum <119345274+leahseyoum@users.noreply.github.com>
2024-11-16 14:59:29 -0800

    initial commit

---
+++ src/main/java/com/teamtreehouse/techdegree/overboard/exc/AnswerAcceptanceException.java
@@ -0,0 +1,7 @@
+package com.teamtreehouse.techdegree.overboard.exc;
+
+public class AnswerAcceptanceException extends RuntimeException {
+    public AnswerAcceptanceException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/teamtreehouse/techdegree/overboard/exc/VotingException.java
@@ -0,0 +1,7 @@
+package com.teamtreehouse.techdegree.overboard.exc;
+
+public class VotingException extends RuntimeException {
+    public VotingException(String message) {
+        super(message);
+    }
+}
